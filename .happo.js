a21c837769ad93c47fd01af4db6af3d44eb60d14
cgal7835 <Chathura.Galappaththi@syscolabs.com>
2020-05-26 13:27:13 +0530

    happo-ci script updated

--- .happo.js
+++ .happo.js
@@ -4,6 +4,10 @@ module.exports = {
     apiKey: "320a01a6c0",
     apiSecret: "b8b640a5da1fad7e158bba559",
 
+    pages: [
+        { url: 'https://daf740eb.ngrok.io', title: 'Localhost' },
+    ],
+
     targets: {
         'chrome-desktop': new RemoteBrowserTarget('chrome', {
             viewport: '1024x768',
